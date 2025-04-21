6034b6f99697c896c95af2837f0f3cbacd17f8aa
Mikaela Gonda <mikaela.gonda@student.ateneo.edu>
2025-04-22 03:39:48 +0800

    0009 - completed 'The user interface'

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Product
+from .models import Product, CartItem
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.contrib.auth import (
@@ -21,12 +22,26 @@ def index(request):
 
 @login_required
 def product_detail(request, product_id):
-    template = loader.get_template("core/product_detail.html")
-    p = Product.objects.get(id=product_id)
-    context = {
-        "product": p
-    }
-    return HttpResponse(template.render(context, request))
+    if request.method == 'GET':
+        template = loader.get_template("core/product_detail.html")
+        p = Product.objects.get(id=product_id)
+        context = {
+            "product": p
+        }
+        return HttpResponse(template.render(context, request))
+    elif request.method == 'POST':
+        submitted_quantity = request.POST['quantity']
+        submitted_product_id = request.POST['product_id']
+        product = Product.objects.get(id=submitted_product_id)
+        user = request.user
+        cart_item = CartItem(user=user, product=product, quantity=submitted_quantity)
+        cart_item.save()
+        messages.add_message(
+            request,
+            messages.INFO,
+            f'Added {submitted_quantity} of {product.name} to your cart'
+        )
+        return redirect('index')
 
 def login_view(request):
     if request.method == 'GET':
