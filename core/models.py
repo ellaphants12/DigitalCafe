71538f001f10bee5d2dac40ace4abfe8010378bc
Mikaela Gonda <mikaela.gonda@student.ateneo.edu>
2025-04-22 03:24:51 +0800

    0009 - finished 'Start with the model;

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,3 +8,10 @@ class Product(models.Model):
     price = models.IntegerField()
     def __str__(self):
         return f'{self.name}'
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
+    quantity = models.IntegerField()
+    def __str__(self):
+        return f'{self.quantity} of {self.product} (User: {self.user.username})'
